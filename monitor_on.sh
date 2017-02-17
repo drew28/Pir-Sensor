289d464252dd585951f3372c63848cccf6ab4f4b
drew28 <atreid@gmail.com>
2017-02-16 19:47:21 -0500

    Update monitor_on.sh

--- monitor_on.sh
+++ monitor_on.sh
@@ -1,3 +1,3 @@
-xset s reset
-tvservice -p
-xset dpms force on
+tvservice --explicit="DMT 82 HDMI"   # use (prefer) mode after `tvservice -m DMT` or CEA
+fbset -depth 8          # dummy fbset as rpdom advised
+fbset -g 1920 1080 1920 1080 16
